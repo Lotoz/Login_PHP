942899196b215ebaf800c82cd370d9b709f54145
Lotoz <144383551+Lotoz@users.noreply.github.com>
2025-11-28 18:14:05 +0100

    Primera edicion

---
+++ php_V1/auth.php
@@ -0,0 +1,57 @@
+<?php
+//Conexion a base de datos
+//Iniciamos la session
+session_start();    //* Hay que hacer que sea segura
+
+if (isset($_REQUEST['user'])) {
+    if (isset($_REQUEST['password'])) {
+        //Hacer lo de HTML_SANITAZION
+        //!Declaracion de variables
+        $DATABASE_HOST = 'localhost';
+        $DATABASE_USER = 'root'; //! Esto es inseguro, deberia ser un usuario con los permisos minimos funcionales
+        $DATABASE_PASS = '';    //! Inseguro
+        $DATABASE_NAME = 'login-php-v1'; //? Porque es la primera version de esta base de datos -> Incluso, es una BD de prueba
+
+        //* Conexion a la base de datos
+        $connection_mysqli = new mysqli($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+        if ($connection_mysqli->connect_error) {
+            //! En vez de ese servicio devuelve a afuera
+            //* O base de datos fuera de servicio, decir la razon
+            $_SESSION['Error'] = "No se puede comprobar la informacion ingresada en este momentos. Vuelva a intentarlo en unos minutos";
+            //die('Error de conexion' . $connection_mysqli->connect_errno); Es solo para mostrar errores pero mejor lo otro
+            header('Location:./index.php');
+        }
+
+        echo "conexion establecidada. ";
+
+        // Vamos a hacer la query
+        // $_ -> Se manejan con name
+        // habria que comprobar si hubo un intento de XSS y contestar con un mensaje de error reprobatorio
+        $usuario = $_POST['user'];
+        $password = $_POST['password'];
+
+        //Que da hacer la query.
+        //Si el usuario o contrasena no existen se redicciona a index.
+        //Si todo es correcto mandar a inicio
+        //Probar ir haciendo tu propia base de datos mas cercana a tu programa
+
+        $querySQL = "SELECT * from usuarios where idusuario = '$usuario' "; //!Debe hacerse si o si con este modo
+        $resultado = $connection_mysqli->query($querySQL);
+        if ($resultado->num_rows == 0) {
+            $_SESSION['Error'] = "El usuario no existe";
+            header('Location:./index.php');
+        } else {
+            //? Comprobar si la password existe
+            //Si no existe, se redireciona a index.php
+            //Si existe te lleva a inicio.php
+            echo 'Existe';
+        }
+    } else {
+        $_SESSION['Error'] = "No te has identificado pillo.";
+        header('Location:./index.php');
+    }
+} else {
+    $_SESSION['Error'] = "No te has identificado pillo.";
+    header('Location:./index.php');
+}
--- php_V1/index.php
+++ php_V1/index.php
@@ -14,7 +14,7 @@ session_start(); //*Pendiente de hacer la seguridad
     <!-- Bootstrap CSS (CDN) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="shortcut icon" href="./assets/img/scpLogo.png" type="image/x-icon">
-    <!-- //! Custom SCP style Estetica rota, revisar luego <link rel="stylesheet" href="./assets/css/style.css">-->
+    <!-- //! Custom SCP style Estetica rota, revisar luego  <link rel="stylesheet" href="./assets/css/style.css">-->
 
 
 </head>
@@ -23,59 +23,57 @@ session_start(); //*Pendiente de hacer la seguridad
     <div class="fondo">
         <div id="cajaLogin" class="contenedor">
             <div class="formularioEstilo">
-                <div class="row g-3 align-items-center">
-                    <div class="col-md-4 d-flex justify-content-center">
-                        <div class="scp-frame">
-                            <img src="./assets/img/scpLogo.png" alt="SCP Logo" class="logoSCP">
-                        </div>
-
-                        <div class="col-md-8">
-                            <form action="" method="POST" class="d-flex flex-column gap-3">
-                                <div>
-                                    <h2 class="text-center">SCP - FOUNDATION</h2>
-                                </div>
-                                <?php
-                                if (isset($_SESSION['Error'])) {
-                                    echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
-                                    <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                                    <div>
-                                        Usuario o contrasena invalidos
-                                    </div>
-                                    </div>';
-                                    //Quita el alert
-                                    //$_SESSION['Error'] = ""; //*Variable vacia
-                                    unset($_SESSION['Error']);
-                                }
-                                ?>
-                                <div class="labelsInputs">
-                                    <label for="user">Username</label>
-                                    <input type="text" id="user" name="user" class="form-control" placeholder=" User here...">
-                                    <div class="mt-2">
-                                        <p class="alert-scp" id="errorUser" hidden>Usuario inválido</p>
-                                    </div>
-                                </div>
-
-                                <div class="labelsInputs">
-                                    <label for="password">Password</label>
-                                    <input type="password" id="password" name="password" class="form-control"
-                                        placeholder="Password here...">
-                                    <div class="mt-2">
-                                        <p class="alert-scp" id="errorPassword" hidden>Contraseña incorrecta</p>
-                                    </div>
-                                </div>
-
-                                <div class="labelsInputs" id="codeSecure" hidden>
-                                    <label for="codeSecureInput">Code-Secure</label>
-                                    <input type="text" id="codeSecureInput" name="codeSecure" class="form-control">
-                                </div>
-                                <div>
-                                    <button type="submit" id="btnLogin"
-                                        class="boton btn btn-outline-dark  w-100">Login</button>
-                                </div>
-
-                            </form>
-                        </div>
+                <div class=" col-md-4 d-flex align-items-center">
+                    <div class="scp-frame">
+                        <img src="./assets/img/scpLogo.png" alt="SCP Logo" class="logoSCP">
                     </div>
+
+                    <div class="col-md-8">
+                        <form action="auth.php" method="POST" class="d-flex flex-column gap-3">
+                            <div>
+                                <h2 class="text-center">SCP - FOUNDATION</h2>
+                            </div>
+                            <?php
+                            if (isset($_SESSION['Error'])) {
+                                echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
+                                    <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                                    <div>';
+                                echo $_SESSION['Error'];
+                                echo   '</div> </div>';
+                                //Quita el alert
+                                //$_SESSION['Error'] = ""; //*Variable vacia
+                                unset($_SESSION['Error']);
+                            }
+                            ?>
+                            <div class="labelsInputs">
+                                <label for="user">Username</label>
+                                <input type="text" id="user" name="user" class="form-control" placeholder=" User here...">
+                                <div class="mt-2">
+                                    <p class="alert-scp" id="errorUser" hidden>Usuario inválido</p>
+                                </div>
+                            </div>
+
+                            <div class="labelsInputs">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" name="password" class="form-control"
+                                    placeholder="Password here...">
+                                <div class="mt-2">
+                                    <p class="alert-scp" id="errorPassword" hidden>Contraseña incorrecta</p>
+                                </div>
+                            </div>
+
+                            <div class="labelsInputs" id="codeSecure" hidden>
+                                <label for="codeSecureInput">Code-Secure</label>
+                                <input type="text" id="codeSecureInput" name="codeSecure" class="form-control">
+                            </div>
+                            <div>
+                                <button type="submit" id="btnLogin"
+                                    class="boton btn btn-outline-dark  w-100">Login</button>
+                            </div>
+
+                        </form>
+                    </div>
+
                 </div>
             </div>
         </div>
